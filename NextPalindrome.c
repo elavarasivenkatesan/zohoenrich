abf2826dcbdc0634acfab8a4a03931e447d7b2f7
elavarasivenkatesan <35231387+elavarasivenkatesan@users.noreply.github.com>
2018-04-07 17:54:28 +0530

    Create NextPalindrome.c

---
+++ NextPalindrome.c
@@ -0,0 +1,86 @@
+#include<stdio.h>
+#include<stdlib.h>
+int digits(int n)
+{
+    int t,c=0;
+    t=n;
+    while(t!=0)
+    {
+        c++;
+        t=t/10;
+    }
+    return c;
+}
+int palindrome(int n,int x)
+{
+    int power,k;
+    if(reverse(n)==n)
+    {
+        return n;
+    }
+    else
+    {
+        k=pow(10,x/2);
+        n=((n/k)*k)+reverse(n/(k*(10*(x%2))));//to find the palindrome number
+        printf("%d",n);
+        return n;
+    }
+}
+int reverse(int n)
+{
+    int p=0;
+    while(n>0)  
+    {
+        p=(p*10)+(n%10);
+        n=n/10;
+    }
+    return p;
+}
+int nextpalindrome(int n)
+{
+    int x,k,r,a;
+    
+    x=digits(n);
+    if(x==1)
+    {
+        if(n%10==9)
+        {
+            printf("%d",n+2);
+            return 0;
+        }
+        printf("%d",++n);
+        return 0;
+    }
+    if(x==2)
+    {
+        if(n%10==9)
+        {
+            printf("%d",n+2);
+            return 0;
+        }
+        printf("%d",n+11);
+        return 0;
+    }
+    else
+    {
+        n=palindrome(n,x);
+        k=x/2;
+        if(x%2==0)
+        {
+            r=n+(11*pow(10,k-1));
+            printf("%d",r);
+        }
+        else
+        {
+            r=n+(1*pow(10,k));
+            printf("%d",r);
+        }
+    }
+}
+int main()
+{
+   int n;
+   scanf("%d",&n);
+   nextpalindrome(n);
+}
+
